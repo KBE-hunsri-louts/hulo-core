505388b4770f77fe79ca3789ea7fd38a0c5372b7
Louts21 <air-bigboss@web.de>
2021-11-07 20:27:36 +0100

    initial project setup

---
+++ src/main/java/com/huloteam/kbe/KbeApplication.java
@@ -0,0 +1,13 @@
+package com.huloteam.kbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/huloteam/kbe/KbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huloteam.kbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
